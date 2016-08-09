a8bb2158c1e6b4d4b168ffd04e3bce231b641bda
chwech <804699297@qq.com>
2016-08-09 21:40:17 +0800

    还没完成！

--- task0016/main.js
+++ task0016/main.js
@@ -6,6 +6,11 @@
  *    "上海": 40
  * };
  */
+window.onload = function(){
+	addAqiData;
+	renderAqiList;
+	//init;
+}
 var aqiData = {};
 
 /**
@@ -15,26 +20,42 @@ var aqiData = {};
 function addAqiData() {
 	var city_input = document.getElementById('aqi-city-input');
 	var value_input = document.getElementById('aqi-value-input');
-	var handle = function(){
-		
-		if(输入不是中英文字符){
+	var handle = function(reg){
+		//城市名必须为中英文字符
+		var reg = /[^a-z\u4e00-\u9fa5]/gi ;//匹配所有不是中英文的字符
+		if(reg.test(this.value)){
 			alert("您输入的字段不是中英文字符，请重新输入");
-		}else(输入的不是数字){
-			alert("您输入的不是数字，请重新输入");
+		}
+
+	}
+	var handle1 = function(){
+		//空气质量指数必须为整数
+		var reg1 = /\D/g ;
+		if(reg1.test(this.value)){
+			alert("您输入的不是整数，请重新输入");
 		}
 	}
 	city_input.addEventListener("change", handle, false);
-	//change事件处理程序判断用户输入的城市名必须为中英文字符，空气质量指数必须为整数
-	
-	city_input.value //获取文本框的值
-	
-	value_input.value
+	value_input.addEventListener("change", handle1, false);
+	aqiData[city_input.value] = value_input.value;
+	alert(aqiData.city_input.value);
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+	var aqi_table = document.getElementById('aqi-talbe');
+	var table_tr = document.createElement("tr");
+	table_tr.innerHTML("<td>城市</td><td>空气质量</td><td>操作</td>")
+	var table_tr2 = document.createElement("tr");
+	var table_td = document.createElement("td");
+	var td_text = document.createTextNode(aqiData[0].slice(0,3));
+
+	aqi_table.appendChild(table_tr);
+	aqi_table.appendChild(table_tr2);
+	table_tr2.appendChild(table_td);
+	table_td.appendChild(td_text);
 
 }
 
@@ -58,9 +79,10 @@ function delBtnHandle() {
 }
 
 function init() {
-
-  // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+	// 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
+	var add_btn = document.getElementById('add-btn');
+	//add_btn.addEventListener("click" ,addBtnHandle , false);
+  
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
